17264b11814bcabc53254da4bc7a2741e235c01a
Victor Guerra <victorhgrodriguez@gmail.com>
2024-09-17 14:56:34 -0600

    fix route

--- src/herramientas/router.ts
+++ src/herramientas/router.ts
@@ -4,9 +4,9 @@ import * as herramientas from '#herramientas/herramientas'
 
 const router = Router()
 
-router.get('/obtener-herramientas', middleware, herramientas.obtenerHerramientas)
-router.post('/crear-herramienta', middleware, herramientas.crearHerramienta)
-router.patch('/actualizar-herramienta', middleware, herramientas.actualizarHerramienta)
-router.delete('/desactivar-herramienta', middleware, herramientas.desactivarHerramienta)
+router.get('/herramientas', middleware, herramientas.obtenerHerramientas)
+router.post('/herramientas', middleware, herramientas.crearHerramienta)
+router.patch('/herramientas', middleware, herramientas.actualizarHerramienta)
+router.delete('/herramientas', middleware, herramientas.desactivarHerramienta)
 
 export default router
