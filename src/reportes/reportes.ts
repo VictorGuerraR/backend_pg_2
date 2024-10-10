8195b47988327057d9b84f63b8a19bc217badd4e
Victor Guerra <victorhgrodriguez@gmail.com>
2024-10-09 20:58:07 -0600

    add endpoints

--- src/reportes/reportes.ts
+++ src/reportes/reportes.ts
@@ -5,17 +5,20 @@ import { Request, Response } from 'express';
 const consultaReporteDepreciacion = () => db({ ds: 'registros.detalle_servicio' })
   .innerJoin({ m: 'registros.maestro' }, 'ds.cod_maestro', 'm.cod_maestro')
   .innerJoin({ h: 'registros.herramienta' }, 'h.cod_herramienta', 'ds.cod_herramienta')
+  .innerJoin({ ur: 'registros.usuarios' }, 'ur.cod_usuario', 'h.cod_usuario_responsable')
   .select(
     'h.cod_herramienta',
     'h.descripcion',
+    'h.fecha_adquisicion',
     'h.monto',
     'h.codigo_moneda',
-    { servicios_brindados: db.raw('count(ds.cod_detalle_servicio)') },
-    { depreciacion_maquinaria: db.raw('sum(ds.total_depreciacion)') },
-    { porcentaje_depreciacion_herramienta: db.raw('round((sum(ds.total_depreciacion)/ h.monto) * 100,2)') }
+    { usuario_responsable: db.raw("CONCAT(ur.nombres, ' ', ur.apellidos)") },
+    { servicios_brindados: db.raw('COUNT(ds.cod_detalle_servicio)') },
+    { depreciacion_maquinaria: db.raw('SUM(ds.total_depreciacion)') },
+    { porcentaje_depreciacion_herramienta: db.raw('ROUND((SUM(ds.total_depreciacion)/ h.monto) * 100,2)') }
   )
   .where('m.activo', true)
-  .groupBy('h.cod_herramienta')
+  .groupBy('h.cod_herramienta', 'ur.nombres', 'ur.apellidos')
 
 export async function reporteDepreciacion(req: Request, res: Response) {
   try {
@@ -23,13 +26,40 @@ export async function reporteDepreciacion(req: Request, res: Response) {
     res.status(200).json(respuesta)
     logger.info({
       message: 'Respuesta exitosa en reportes:reporteDepreciacion',
-      labels: { code: 200, scope: 'delete', ususario: req.usuario?.usuario }
+      labels: { code: 200, scope: 'get', ususario: req.usuario?.usuario }
     });
   } catch (error) {
     res.status(418).json({ error })
     logger.error({
       message: 'Respuesta con errores en reportes:reporteDepreciacion',
-      labels: { code: 418, scope: 'delete', ususario: req.usuario?.usuario, error }
+      labels: { code: 418, scope: 'get', ususario: req.usuario?.usuario, error }
+    });
+  }
+}
+
+const consultaReporteGananciaPorUsuario = () => db({ m: 'registros.maestro' })
+  .innerJoin({ u: 'registros.usuarios' }, 'u.cod_usuario', 'h.cod_usuario_creacion')
+  .select(
+    { usuario: db.raw("CONCAT(u.nombres, ' ', u.apellidos)") },
+    { ganancia: db.raw('SUM(m.monto_ganacia)') },
+    { impuesto: db.raw('SUM(m.monto_impuesto)') }
+  )
+  .where('m.activo', true)
+
+
+export async function reporteGananciaPorUsuario(req: Request, res: Response) {
+  try {
+    const respuesta = await consultaReporteGananciaPorUsuario()
+    res.status(200).json(respuesta)
+    logger.info({
+      message: 'Respuesta exitosa en reportes:reporteGananciaPorUsuario',
+      labels: { code: 200, scope: 'get', ususario: req.usuario?.usuario }
+    });
+  } catch (error) {
+    res.status(418).json({ error })
+    logger.error({
+      message: 'Respuesta con errores en reportes:reporteGananciaPorUsuario',
+      labels: { code: 418, scope: 'get', ususario: req.usuario?.usuario, error }
     });
   }
 }
--- src/reportes/router.ts
+++ src/reportes/router.ts
@@ -5,5 +5,6 @@ import * as reportes from '#reportes/reportes'
 const router = Router()
 
 router.get('/reportes-depreciacion', middleware, reportes.reporteDepreciacion)
+router.get('/reportes-ganancias', middleware, reportes.reporteGananciaPorUsuario)
 
 export default router
